af9efc1da253eb45cb3cab158ccde8ccda73c37d
mwr <2036827428@qq.com>
2021-03-05 19:50:17 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/routes'
 import store from './store'
 
+import Vant from 'vant'
+// import 'vant/lib/index.css'
+import 'vant/lib/index.less'
+// 引入
+import './styles/index.less'
+import 'amfe-flexible'
+// 注册组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
